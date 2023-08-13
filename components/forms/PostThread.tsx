aa32a15a1bef197eb3f1dff427a0fdec13167921
Abhinay Prakash Reddy <abhinayprakashreddy@Abhinays-MacBook-Air.local>
2023-08-13 23:21:49 +0530

    Added Ai

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,5 +1,88 @@
+// "use client";
+
+// import * as z from "zod";
+// import { useForm } from "react-hook-form";
+// import { useOrganization } from "@clerk/nextjs";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { usePathname, useRouter } from "next/navigation";
+
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Button } from "@/components/ui/button";
+// import { Textarea } from "@/components/ui/textarea";
+
+// import { ThreadValidation } from "@/lib/validations/thread";
+// import { createThread } from "@/lib/actions/thread.actions";
+
+// interface Props {
+//   userId: string;
+// }
+
+// function PostThread({ userId }: Props) {
+//   const router = useRouter();
+//   const pathname = usePathname();
+
+//   const { organization } = useOrganization();
+
+//   const form = useForm<z.infer<typeof ThreadValidation>>({
+//     resolver: zodResolver(ThreadValidation),
+//     defaultValues: {
+//       thread: "",
+//       accountId: userId,
+//     },
+//   });
+
+//   const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+//     await createThread({
+//       text: values.thread,
+//       author: userId,
+//       communityId: organization ? organization.id : null,
+//       path: pathname,
+//     });
+
+//     router.push("/");
+//   };
+
+//   return (
+//     <Form {...form}>
+//       <form
+//         className='mt-10 flex flex-col justify-start gap-10'
+//         onSubmit={form.handleSubmit(onSubmit)}
+//       >
+//         <FormField
+//           control={form.control}
+//           name='thread'
+//           render={({ field }) => (
+//             <FormItem className='flex w-full flex-col gap-3'>
+//               <FormLabel className='text-base-semibold text-light-2'>
+//                 Content
+//               </FormLabel>
+//               <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+//                 <Textarea rows={15} {...field} />
+//               </FormControl>
+//               <FormMessage />
+//             </FormItem>
+//           )}
+//         />
+
+//         <Button type='submit' className='bg-primary-500'>
+//           Post Thread
+//         </Button>
+//       </form>
+//     </Form>
+//   );
+// }
+
+// export default PostThread;
 "use client";
 
+import React, { useState } from "react";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { useOrganization } from "@clerk/nextjs";
@@ -20,6 +103,8 @@ import { Textarea } from "@/components/ui/textarea";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
 
+const axios = require("axios");
+
 interface Props {
   userId: string;
 }
@@ -27,6 +112,10 @@ interface Props {
 function PostThread({ userId }: Props) {
   const router = useRouter();
   const pathname = usePathname();
+  const [hashtags, setHashtags] = useState<string[]>([]);
+  const [correctedText, setCorrectedText] = useState<string>("");
+  const [loading, setloading] = useState(false);
+  const [Loading, setLoading] = useState(false);
 
   const { organization } = useOrganization();
 
@@ -37,6 +126,67 @@ function PostThread({ userId }: Props) {
       accountId: userId,
     },
   });
+
+  const generateHashtags = async (values: z.infer<typeof ThreadValidation>) => {
+    setLoading(true);
+    try {
+      const options = {
+        method: "POST",
+        url: "https://chatgpt-api7.p.rapidapi.com/ask",
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key":
+            "ce222f844dmshe102f93562134c4p184e64jsn78b106510366",
+          "X-RapidAPI-Host": "chatgpt-api7.p.rapidapi.com",
+        },
+        data: {
+          query: "Generate relatable hashtags for my post: " + values.thread,
+        },
+      };
+
+      const response = await axios.request(options);
+      console.log(response);
+      const generatedHashtags = response.data.response;
+
+      if (generatedHashtags && typeof generatedHashtags === "string") {
+        const hashtagArray = generatedHashtags.split("\n");
+        setHashtags(hashtagArray);
+      } else {
+        console.error("Invalid generated hashtags data:", generatedHashtags);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false); // Set loading state back to false after API call, whether successful or not
+    }
+  };
+
+  const correctMistakes = async (values: z.infer<typeof ThreadValidation>) => {
+    setloading(true);
+    try {
+      const options = {
+        method: "POST",
+        url: "https://chatgpt-api7.p.rapidapi.com/ask",
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key":
+            "ce222f844dmshe102f93562134c4p184e64jsn78b106510366",
+          "X-RapidAPI-Host": "chatgpt-api7.p.rapidapi.com",
+        },
+        data: {
+          query: "Correct the mistakes in my post: \n" + values.thread,
+        },
+      };
+
+      const response = await axios.request(options);
+      console.log(response);
+      setCorrectedText(response.data.response);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setloading(false); // Set loading state back to false after API call, whether successful or not
+    }
+  };
 
   const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
     await createThread({
@@ -52,18 +202,18 @@ function PostThread({ userId }: Props) {
   return (
     <Form {...form}>
       <form
-        className='mt-10 flex flex-col justify-start gap-10'
+        className="mt-10 flex flex-col justify-start gap-10"
         onSubmit={form.handleSubmit(onSubmit)}
       >
         <FormField
           control={form.control}
-          name='thread'
+          name="thread"
           render={({ field }) => (
-            <FormItem className='flex w-full flex-col gap-3'>
-              <FormLabel className='text-base-semibold text-light-2'>
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormLabel className="text-base-semibold text-light-2">
                 Content
               </FormLabel>
-              <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
                 <Textarea rows={15} {...field} />
               </FormControl>
               <FormMessage />
@@ -71,7 +221,56 @@ function PostThread({ userId }: Props) {
           )}
         />
 
-        <Button type='submit' className='bg-primary-500'>
+        <Button
+          className="bg-sky-400"
+          type="button"
+          onClick={form.handleSubmit(generateHashtags)}
+        >
+          Generate Relatable Hashtags
+        </Button>
+        {Loading ? (
+          <div className="bg-light-1 p-4 rounded-md animate-pulse">
+            <div className="h-4 bg-gray-300 rounded w-16 mb-2"></div>
+            <p className="text-dark-1 ml-96 mb-2">Loading....</p>
+            <div className="h-4 bg-gray-300 rounded w-24"></div>
+          </div>
+        ) : (
+          hashtags.length > 0 && (
+            <div className="bg-light-1 p-4 rounded-md mt-4">
+              <p className="text-dark-1">Generated Hashtags:</p>
+              <ul className="mt-2">
+                {hashtags.map((tag, index) => (
+                  <li key={index} className="text-dark-2">
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )
+        )}
+
+        <Button
+          className="bg-green-400"
+          type="button"
+          onClick={form.handleSubmit(correctMistakes)}
+        >
+          Correct Mistakes in Post
+        </Button>
+
+        {loading ? (
+          <div className="bg-light-1 p-4 rounded-md animate-pulse">
+            <div className="h-4 bg-gray-300 rounded w-16 mb-2"></div>
+            <p className="text-dark-1 ml-96 mb-2">Loading....</p>
+            <div className="h-4 bg-gray-300 rounded w-24"></div>
+          </div>
+        ) : correctedText ? (
+          <div className="bg-light-1 p-4 rounded-md">
+            <p>Corrected Text:</p>
+            <p className="text-dark-1">{correctedText}</p>
+          </div>
+        ) : null}
+
+        <Button type="submit" className="bg-primary-500">
           Post Thread
         </Button>
       </form>
